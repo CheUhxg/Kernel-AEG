9f1dbd4f97641f91dd98dd6b49e76001b422a537
CheUhxg <3193079152@qq.com>
2025-01-02 18:12:38 +0800

    update: upload util.h

---
+++ identifier/test/util.h
@@ -0,0 +1,33 @@
+struct Type {
+    int len;
+    int *ptr;
+    void *buf;
+};
+
+struct Bridge {
+    int len;
+    char buf[];
+};
+
+struct Router {
+    int *ptr;
+    void *buf;
+};
+
+void *USER_SPACE;
+
+void *kmalloc(unsigned size) {
+    return &size;
+}
+
+void memcpy(void *to, void *from, unsigned size) {
+    to = (char *)from + size;
+}
+
+void copy_from_user(void *to, void *from, unsigned size) {
+    to = (char *)from + size;
+}
+
+void *get_heap_buf(unsigned len) {
+    return kmalloc(len);
+}
